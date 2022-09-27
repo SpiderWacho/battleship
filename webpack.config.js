13f383ef1a28367c7c2c70077cd70d8b74d92633
SpiderWacho <iamgaas@gmail.com>
2022-09-26 22:08:38 -0300

    Commit new build

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Battleship',
       template: './src/index.html'
     }),
   ],
